fcda8724a97120b51c7b5d092381c02bc89ede84
mohammed kaif <mkaif555123@gmail.com>
2024-11-03 15:26:12 +0500

    filter and forEach

---
+++ Arrays Methods/script.js
@@ -0,0 +1,19 @@
+// for each
+
+// const eachNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const forEachNums = eachNumbers.forEach((num) => {
+//     return num > 4
+// })
+
+// console.log(forEachNums);
+
+// filters
+
+const filterNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filterNums = filterNumbers.filter((fil) => {
+    return fil > 4
+})
+
+console.log(filterNums);
