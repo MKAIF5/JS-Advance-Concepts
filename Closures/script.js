6f5997a30c7d7049fd3ee5d0e4400b2d3a01ccaf
mohammed kaif <mkaif555123@gmail.com>
2024-11-08 19:20:53 +0500

    clousers string and number practice done

---
+++ Closures/script.js
@@ -0,0 +1,28 @@
+// const main = (name) => {
+//     const displayName = () => {
+//         console.log(name);
+//     }
+//     return displayName
+// }
+// const fn = main("kaif");
+// fn()
+// fn()
+// fn()
+// fn()
+// fn()
+// fn()
+// fn()
+
+// add in clousures
+
+// const adder = (num) => {
+//     const add = (a) => {
+//         console.log(num + a);
+//     }
+//     return add
+// }
+
+// const addTo5 = adder(5);
+// addTo5(10)
+// addTo5(3)
+// addTo5(676)
